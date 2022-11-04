c70a8f03318bf89dd0de9e98bfca2faea6114d05
Adebanjo Adeniji <aeben.adebanjo@gmail.com>
2022-11-04 03:09:21 +0100

    align card and set responsiveness

--- src/components/CardComponents/StatsCard.js
+++ src/components/CardComponents/StatsCard.js
@@ -5,69 +5,15 @@ import { FaWallet, FaEye, FaPercent, FaMoneyBill } from "react-icons/fa";
 const StatsCard = () => {
   return (
     <section>
-      {/* <div className="StatsCard color1">
-        <div className="total-loans">
-          <div className="logo">
-            <FaWallet />
-          </div>
-          <div className="eye">
-            <FaEye />
-          </div>
-          <div className="content">
-            <div className="total">
-              <h6>Total loans amount</h6>
-            </div>
-            <h5>
-              <span className="currency">&#8358;</span>150, 000.00
-            </h5>
-          </div>
-        </div>
-      </div>
-      <div className="StatsCard color2">
-        <div className="total-loans">
-          <div className="logo">
-            <FaPercent />
-          </div>
-          <div className="eye">
-            <FaEye />
-          </div>
-          <div className="content">
-            <div className="total">
-              <h6>Total repaid amount</h6>
-            </div>
-          </div>
-          <h5>
-            <span className="currency">&#8358;</span>15, 000.00
-          </h5>
-        </div>
-      </div>
-      <div className="StatsCard color3">
-        <div className="total-loans">
-          <div className="logo">
-            <FaMoneyBill />
-          </div>
-          <div className="eye">
-            <FaEye />
-          </div>
-          <div className="content">
-            <div className="total">
-              <h6>Loan balance</h6>
-            </div>
-            <h5>
-              <span className="currency">&#8358;</span>135, 000.00
-            </h5>
-          </div>
-        </div>
-      </div> */}
       <div className="container text-center">
-        <div className="row">
-          <div className="col-4 ">
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-6 ">
             <div className="total-loans">
-              <div className="logo">
+              <div className="logo1">
                 <FaWallet />
               </div>
               <div className="content">
-                <div className="total">
+                <div className="total1">
                   <h6>Total loans amount</h6>
                 </div>
                 <h5>
@@ -79,13 +25,13 @@ const StatsCard = () => {
               </div>
             </div>
           </div>
-          <div class="col-4 col-md-4">
+          <div className="col-lg-4 col-md-6">
             <div className="total-repaid">
-              <div className="logo">
+              <div className="logo2">
                 <FaPercent />
               </div>
               <div className="content">
-                <div className="total">
+                <div className="total2">
                   <h6>Total repaid amount</h6>
                 </div>
                 <h5>
@@ -97,13 +43,13 @@ const StatsCard = () => {
               </div>
             </div>
           </div>
-          <div class="col-4 col-md-4">
+          <div className="col-lg-4 col-md-6">
             <div className="loan-balance">
-              <div className="logo">
+              <div className="logo3">
                 <FaMoneyBill />
               </div>
               <div className="content">
-                <div className="total">
+                <div className="total3">
                   <h6>Loan balance</h6>
                 </div>
                 <h5>
